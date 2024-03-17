fdbd9e37996eed3841e954dc12b3d5c25cbeb482
Islam Turky <iturky06#gmail.com>
2024-03-17 13:49:07 +0200

    sam1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,48 @@
+import { Box, Typography, Divider } from '@mui/material'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
 const Footer = () => {
     return(
-        <div id="Footer">
-            جميع الحقوق محفوظه <span id="copy">&copy;</span> مركز صيانة كريازي
-        </div>
+        <>
+        <Box id="Footer">
+            <div className="flogo">
+                <img src="/Samsung_Logo.svg" alt="Samsung_Logo" />
+                <Typography variant='p'>مجموعة مراكز صيانة سامسونج فى مصر تقدم خدماتها لصيانة أجهزة سامسونج ما بعد فترة الضمان.</Typography>
+            </div>
+            <div className="tcontent">
+                <div className='it'>
+                    <Typography variant='h4' sx={{ color: 'white' }}>صيانة سامسونج</Typography>
+                    <div className='lis'>
+                        <a href="#">الشروط و الأحكام</a>
+                        <a href="#">رؤية الشركة</a>
+                        <a href="#">شكاوى و مقترحات</a>
+                    </div>
+                </div>
+                <div className='it'>
+                    <Typography variant='h4' sx={{ color: 'white' }}>عن الشركة</Typography>
+                    <div className='lis'>
+                        <a href="#">وظائف</a>
+                        <a href="#">طاقم العمل</a>
+                        <a href="#">خدمات مميزة</a>
+                    </div>
+                </div>
+                <div className='it'>
+                    <Typography variant='h4' sx={{ color: 'white', textAlign: 'right' }}>تابعونا على</Typography>
+                    <div className='list'>
+                        <FacebookIcon sx={{ fontSize: '50px' }}/>
+                        <InstagramIcon sx={{ fontSize: '50px' }}/>
+                        <WhatsAppIcon sx={{ fontSize: '50px' }}/>
+                        <TwitterIcon sx={{ fontSize: '50px' }}/>
+                    </div>
+                </div>
+            </div>
+        </Box>
+        <Divider />
+        <div className='endF' style={{ background: 'GREY', fontSize: '20px', padding: '5px', textAlign: 'center', color: 'white' }}>جميع الحقوق محفوظة صيانة سامسونج © 2018</div>
+        </>
     )
 }
 
