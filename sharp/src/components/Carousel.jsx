82d1380d37882c2f724ca31f3213808aa60cce8f
Islam Turky <iturky06@gmail.com>
2024-11-15 16:00:22 +0200

    wid

--- sharp/src/components/Carousel.jsx
+++ sharp/src/components/Carousel.jsx
@@ -12,19 +12,25 @@ const MyCarousel = () => {
                 <Carousel.Item >
                     <img src={img1} style={{ opacity: '0.5' }}/>
                     <div className='position-absolute d-flex justify-content-center align-items-center align-content-center' style={{  width: '100%', top: '40%' }}>
-                        <TextTyping text='مرحبا بك في مركز صيانة شارب' />
+                        <div className='w-75 text-center'>
+                            <TextTyping text='مرحبا بك في مركز صيانة شارب' />
+                        </div>
                     </div>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img src={img2} style={{ opacity: '0.5' }} />
                     <div className='position-absolute d-flex justify-content-center align-items-center align-content-center' style={{  width: '100%', top: '40%' }}>
-                        <TextTyping text='لدينا أفضل الفنيين المتخصصين في صيانة جميع الأجهزه المنزليه' />
+                        <div className='w-75 text-center'>
+                            <TextTyping text='لدينا أفضل الفنيين المتخصصين في صيانة جميع الأجهزه المنزليه' />
+                        </div>
                     </div>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img src={img3} style={{ opacity: '0.5' }} />
                     <div className='position-absolute d-flex justify-content-center align-items-center align-content-center' style={{  width: '100%', top: '40%' }}>
-                        <TextTyping text='نمتاز بالسرعه في صيانة الأجهزه المنزليه' />
+                        <div className='w-75 text-center'>
+                            <TextTyping text='نمتاز بالسرعه في صيانة الأجهزه المنزليه' />
+                        </div>
                     </div>
                 </Carousel.Item>
             </Carousel>
